e80ef72a93e2aea78915bca84873dbd99b737950
Renan Coutinho <renan.coutinho.11@hotmail.com>
2025-05-22 22:33:00 -0300

    update2

--- Date/Program.cs
+++ Date/Program.cs
@@ -4,6 +4,12 @@ using System.Data;
 using System.Globalization;
 
 Console.Clear();
+
+var timeSpan = new TimeSpan(5, 12, 00);
+Console.WriteLine(timeSpan);
+
+
+
 // var data = DateTime.Now;
 
 /*Console.WriteLine(data);
@@ -24,6 +30,20 @@ var de = new CultureInfo("de-DE");
 //data local da maquina , servidor.
 var atual = CultureInfo.CurrentCulture;
 
+//horario base
+var utcDate = DateTime.UtcNow;
+
+var timezoneAustralia = TimeZoneInfo.FindSystemTimeZoneById("Pacific/Auckland");
+Console.WriteLine(timezoneAustralia);
+
+var horaAustralia = TimeZoneInfo.ConvertTimeFromUtc(utcDate, timezoneAustralia);
+Console.WriteLine(horaAustralia);
+
+Console.WriteLine("--------------------------------------");
+
+Console.WriteLine(utcDate);
+Console.WriteLine(utcDate.ToLocalTime());
+
 Console.WriteLine(data.ToString("D", pt));
 Console.WriteLine(data.ToString("D", de));
 
@@ -36,6 +56,14 @@ Console.WriteLine(data.AddYears(1));
 if (data.Date >= DateTime.Now.Date) { Console.WriteLine("é Igual"); } else { Console.WriteLine("Difetente"); }
 
 
+var timezones = TimeZoneInfo.GetSystemTimeZones();
+foreach (var timezone in timezones)
+{
+    Console.WriteLine(timezone.Id);
+    Console.WriteLine(timezone);
+    Console.WriteLine(TimeZoneInfo.ConvertTimeFromUtc(utcDate, timezone));
+    Console.WriteLine("---------------");
+}
 // Y ou y = ano; M = mes; d = dia; h = hora; s = segundo 
 //var formatada = string.Format("{0:dd-MM-yyyy / hh:mm:ss }", data);
 //Console.WriteLine(formatada);
@@ -46,3 +74,14 @@ if (data.Date >= DateTime.Now.Date) { Console.WriteLine("é Igual"); } else { Co
 //var formatada3 = string.Format("{0:f}", data);
 //Console.WriteLine(formatada3);
 
+//metodos
+Console.WriteLine(DateTime.DaysInMonth(2025, 5));
+
+static bool IsWeekend(DayOfWeek today)
+{
+    return today == DayOfWeek.Saturday || today == DayOfWeek.Sunday;
+
+}
+Console.WriteLine(IsWeekend(DateTime.Now.DayOfWeek));
+
+Console.WriteLine(DateTime.Now.IsDaylightSavingTime());
