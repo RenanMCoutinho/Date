4c18b7aac3ab4b8d11b17ae8f935cb3648d4ca2b
Renan Coutinho <renan.coutinho.11@hotmail.com>
2025-05-22 22:03:19 -0300

    Adicionar arquivos de projeto.

---
+++ Date/Program.cs
@@ -0,0 +1,39 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System.Data;
+
+Console.Clear();
+// var data = DateTime.Now;
+
+/*Console.WriteLine(data);
+Console.WriteLine(data.Year);
+Console.WriteLine(data.Month);
+Console.WriteLine(data.Day);
+Console.WriteLine(data.Hour);
+Console.WriteLine(data.Minute);
+Console.WriteLine(data.Second);
+Console.WriteLine(data.TimeOfDay);
+Console.WriteLine(data.DayOfWeek);*/
+
+
+var data = DateTime.Now;
+
+
+Console.WriteLine(data);
+Console.WriteLine(data.AddDays(-1));
+Console.WriteLine(data.AddMonths(12));
+Console.WriteLine(data.AddYears(1));
+
+if (data.Date >= DateTime.Now.Date) { Console.WriteLine("é Igual"); } else { Console.WriteLine("Difetente"); }
+
+
+// Y ou y = ano; M = mes; d = dia; h = hora; s = segundo 
+//var formatada = string.Format("{0:dd-MM-yyyy / hh:mm:ss }", data);
+//Console.WriteLine(formatada);
+
+//var formatada2 = string.Format("{0:D}", data);
+//Console.WriteLine(formatada2);
+
+//var formatada3 = string.Format("{0:f}", data);
+//Console.WriteLine(formatada3);
+
